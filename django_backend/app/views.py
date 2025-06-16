9e5a19f594b157d667ad4f34bdbaafd7e4367499
tinwan.ng <tinwan.ng@govmind.tech>
2025-06-16 14:45:51 +0200

    feat: make sort possible via default and other 3 fields

--- django_backend/app/views.py
+++ django_backend/app/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView
 from .models import *
 from .serializers import *
+from django.db.models import F
 
 """
 state the allowed parameters for the endpoints here in views
@@ -16,7 +17,13 @@ class AnimeListView(ListAPIView):
         if genre:
             queryset = queryset.filter(genres__name__iexact=genre)
 
-        if order_by in ['title', '-title', 'score', '-score']:
-            queryset = queryset.order_by(order_by)
+        sort_vals = ['title','score', 'popularity', 'year']  # only allow specific whitelisted values
+        if order_by in sort_vals + ['-' + val for val in sort_vals]:
+            if "title" in order_by:
+                queryset = queryset.extra(select={'title_default': "json_extract(titles, '$.Default')"}).order_by('title_default')
+                if order_by.startswith("-"):
+                    queryset = queryset.reverse()
+            else:
+                queryset = queryset.order_by(order_by)
 
         return queryset

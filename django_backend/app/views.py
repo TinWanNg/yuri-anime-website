824241b139e40beb7f0bc7b1fefd79c518ed7887
tinwan.ng <tinwan.ng@govmind.tech>
2025-06-16 16:57:49 +0200

    feat: add param filter for endpoint

--- django_backend/app/views.py
+++ django_backend/app/views.py
@@ -9,15 +9,28 @@ state the allowed parameters for the endpoints here in views
 class AnimeListView(ListAPIView):
     serializer_class = AnimeSerializer
 
+    # only allow specific whitelisted values for filter and sort
     def get_queryset(self):
         queryset = Anime.objects.all()
+
+        # Filter
         genre = self.request.query_params.get('genre')
-        order_by = self.request.query_params.get('sort')
+        type = self.request.query_params.get('type')
+        status = self.request.query_params.get('status')
 
         if genre:
-            queryset = queryset.filter(genres__name__iexact=genre)
+            queryset = queryset.filter(genres__id=genre)
 
-        sort_vals = ['title','score', 'popularity', 'year']  # only allow specific whitelisted values
+        # handle text chocies fields
+        if type and Type(type):
+            queryset = queryset.filter(type__iexact=type)
+        if status and Status(status):
+            queryset = queryset.filter(status__iexact=status)
+
+        # Sort
+        order_by = self.request.query_params.get('sort')
+
+        sort_vals = ['title','score', 'popularity', 'year']  
         if order_by in sort_vals + ['-' + val for val in sort_vals]:
             if "title" in order_by:
                 queryset = queryset.extra(select={'title_default': "json_extract(titles, '$.Default')"}).order_by('title_default')
